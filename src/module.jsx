90c9452baa8aa26c00f361ccf7f62c82ec62c2de
Dmitri Gorun <iii448@mail.ru>
2022-07-13 18:31:08 +0500

    first commit

---
+++ src/module.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import st from './img/st.jpg';
+
+const Module = () => (
+  <>
+    <h1>Hellow React!!</h1>
+    <img src={st} width={600} />
+  </>
+);
+
+export default Module;
+
+
+
+
+
+
